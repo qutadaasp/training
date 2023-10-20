b12c614eba8d1e342a919311b6ce6793778a3635
qutadaasp <qutadaadnan@hotmail.com>
2023-10-20 15:25:27 +0300

    sss

---
+++ WebApplication6/Models/ApplicationDBcontext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication6.Models
+{
+    public class ApplicationDBcontext : DbContext
+    {
+        public ApplicationDBcontext(DbContextOptions<ApplicationDBcontext> options) : base(options)
+        {
+
+        }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
